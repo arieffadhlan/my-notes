892fe74b0a2274dd8fe96866756b216c8f0cbf6f
Muhammad Arief Fadhlan <ariffadhlan12@gmail.com>
2022-07-20 19:59:39 +0700

    initial commit

---
+++ src/pages/MyNotes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/style.css';
+
+class MyNotes extends React.Component {
+    render() {
+        return (
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+export default MyNotes;
